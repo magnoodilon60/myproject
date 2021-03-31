f3b0ea3595d3a2f41ceb4545e225d75f4ccf286c
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-31 10:34:38 -0300

    teste

---
+++ palm/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def hello_world(username):
+    # mostrar o perfil do usuário para aquele usuário
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def porj(post_id):
+    # mostra a postagem com o id fornecido, o id é um inteiro
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/projects/')
+def projects():
+    return 'O primeiro projeto'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+app.run(port=5000, debug=True)
