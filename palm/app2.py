1a4a468fb929c180646586263c887a4ce5349dab
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-31 14:53:54 -0300

    uso do url for no flask

--- palm/app2.py
+++ palm/app2.py
@@ -1,3 +1,32 @@
 #teste
 #algum coteudo novo
 
+from flask import Flask, url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+#criar primeira rota
+@app.route('/')
+def index():
+    return 'index'
+
+
+#segunda rota
+@app.route('/login')
+def login():
+    return 'login'
+
+#terceira rota
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile'. username='Jhon Doe'))
+
+
